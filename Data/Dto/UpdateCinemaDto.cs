8ea8f9be905e71cff34f594621dd4428fa26a2ec
Alexandre Luchetti <106784981+alexandreluchetti@users.noreply.github.com>
2023-03-27 20:01:20 -0300

    Delete Data directory

--- Data/Dto/UpdateCinemaDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace MoviesAPI.Data.Dto;
-
-public class UpdateCinemaDto
-{
-    [Required(ErrorMessage = "O campo de nome é obrigatório.")]
-    public string Name { get; set; }
-}
